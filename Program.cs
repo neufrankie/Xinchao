df69a097b39af6ac872d71de7897eed45c479dc2
neufrankie <neu.vietanh272@gmail.com>
2024-05-25 20:07:00 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nhập tên từ người dùng
+            Console.Write("Ban ten la gi? ");
+            string ten = Console.ReadLine();
+
+            // Hiển thị lời chào kèm theo tên
+            Console.WriteLine("Xin chao " + ten + " chuc mot ngay moi tot lanh!");
+        }
+    }
+}
